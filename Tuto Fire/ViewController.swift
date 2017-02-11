355e6364d49f14d751206d9fe6b508896c921a20
MAC <mac@macs-macbook-pro.local>
2017-02-11 19:07:28 +0000

    Initial Commit

---
+++ Tuto Fire/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Tuto Fire
+//
+//  Created by Yassine on 11/02/2017.
+//  Copyright © 2017 Yassine EN. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
